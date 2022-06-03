02b7782ef499c5df41684ec422213ebb9b2e4505
Romain Fouilland <romain.fd@gmail.com>
2022-06-03 18:39:24 +0200

    Collecting companies

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-const indeed = require('./indeed-scrapper/indeed-scraper');
+const indeed = require('./indeed-scraper');
+const consola = require('consola');
+const fs = require('fs');
+
+const queryOptions = {
+    host: 'fr.indeed.com',
+    query: 'Scaleway',
+    city: 'France',
+    radius: '25',
+    // level: 'entry_level',
+    // jobType: 'fulltime',
+    // maxAge: '7',
+    // sort: 'date',
+    // limit: 10
+};
+
+// "AWS", "GCP", "Azure", "Scaleway"
+["GCP", "Azure"].forEach(csp => {
+    consola.info("Running indeed-scraper for " + csp)
+    queryOptions['query'] = csp
+    indeed.query(queryOptions).then(async jobs => {
+        consola.info("Collecting companies infos for " + csp)
+        const companies = await indeed.getCompanies(jobs)
+        // const companies = {}
+        fs.writeFile(
+            "outputs/" + csp + ".json",
+            JSON.stringify({jobs, companies}),
+            err => {if (err) {console.log(err);}}
+        );
+    });
+})
